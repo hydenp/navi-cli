ee7d2b170e21612341783e4adeeb2521ad462b33
Hyden Polikoff <hyden.dev@gmail.com>
2022-02-11 19:27:41 -0700

    update setup

--- setup.py
+++ setup.py
@@ -3,15 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='navi-cli',
     version='0.1.0',
-    py_modules=['nav'],
-    packages=find_packages(),
-    include_package_data=True,
+    description='See the Github repo',
+    author="Real Python",
+    author_email="hyden.dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
     install_requires=[
         'Click',
     ],
+    py_modules=['nav'],
+    packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
-            'navi = nav.__main__:main',
+            'navi=nav.__main__:main',
         ],
     },
 )
